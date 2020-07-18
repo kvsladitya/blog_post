3a5670685ceef9f810000f10f352209a009635b7
kvsladitya <kvsladitya@outlook.com>
2020-07-18 10:12:58 +0530

    testing heroku

---
+++ posts/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(queryset=User.objects.all(),slug_field='username')
+    class Meta:
+        model = Post
+        fields = ('id','author','title','body','created_at',)
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id','username')
